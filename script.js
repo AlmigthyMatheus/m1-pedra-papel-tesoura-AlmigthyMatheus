616606da65789171a95761615687102d2593e4a2
Matheus Brandao <patativagames@gmail.com>
2024-02-20 22:41:53 -0300

    Update script.js
    Projeto | 🏁 Pedra Papel Tesoura

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2) {
+  const pedra = "Pedra";
+  const papel = "Papel";
+  const tesoura = "Tesoura";
+
+  if (
+    (player1 === pedra && player2 === papel) ||
+    (player1 === papel && player2 === tesoura) ||
+    (player1 === tesoura && player2 === pedra)
+  ) {
+    return "Jogador 2 venceu!";
+  } else if (
+    (player2 === pedra && player1 === papel) ||
+    (player2 === tesoura && player1 === pedra) ||
+    (player2 === papel && player1 === tesoura)
+  ) {
+    return "Jogador 1 venceu!";
+  }
+
+  return "Empate!";
+}
